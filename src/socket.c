87caf720f5518e088696f763305a2d76bc893048
Luigi <maxime.opsommer@gmail.com>
2015-02-16 21:26:23 +0100

    tentative setsockopt

--- src/socket.c
+++ src/socket.c
@@ -56,6 +56,10 @@ int creer_serveur(int port){
 		perror("socket serveur");
 		return -1;
 	}
+
+	/*int optval = 1;
+	if(setsockopt(socket_serveur, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int)) == -1)
+		perror("Can not set  SO_REUSEADDR option");*/
 	return config_socket(socket_serveur); 	
 }
 
