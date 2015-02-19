3a25c6bcc32228b9fb1d7656d46fe94caf94b9ac
Camille BREUZON <camillebreuzon@orange.fr>
2015-02-19 13:56:30 +0100

    setsockopt

--- src/socket.c
+++ src/socket.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <string.h>
 #include <sys/types.h> 
@@ -8,6 +7,7 @@
 #include <netinet/ip.h>
 #include <unistd.h>
 #include <pthread.h>
+
 /*
 int accepter(int socket_serveur){
 	int socket_client;
@@ -36,11 +36,6 @@ int config_socket(int socket_serveur){
 	saddr.sin_family = AF_INET; /* Socket ipv4 */
 	saddr.sin_port = htons(8080); /* Port d ’ écoute */
 	saddr.sin_addr.s_addr = INADDR_ANY; /* écoute sur toutes les interfaces */
-	
-	int optval = 1;
-	if (setsockopt(socket_serveur, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int)) == -1)
-				perror ("Can not set SO_REUSEADDR option");
-
 	if(bind(socket_serveur, (struct sockaddr *)&saddr, sizeof(saddr)) == -1){
 		perror("bind socket");
 		return -1;
@@ -51,26 +46,12 @@ int config_socket(int socket_serveur){
 
 int creer_serveur(int port){
 	int socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
-
 	if(socket_serveur == -1){
 		perror("socket serveur");
 		return -1;
 	}
-
-	/*int optval = 1;
-	if(setsockopt(socket_serveur, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int)) == -1)
-		perror("Can not set  SO_REUSEADDR option");*/
+	int optval = 1;
+	if (setsockopt(socket_serveur, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int)) == -1)
+		perror ("Can not set SO_REUSEADDR option");
 	return config_socket(socket_serveur); 	
 }
-
-
-
-
-
-
-
-
-
-
-
-
